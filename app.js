2c909f5e88804db106ffbb6e3dec2ec015a4f51b
alexcaribou83 <alex.caribou83@gmail.com>
2026-08-07 08:18:05 +0200

    Update app.js

--- app.js
+++ app.js
@@ -371,14 +371,13 @@ function displaySongs() {
         <div class="song-info">
 
 
-            <h3>
-                ${escapeHTML(song.title)}
-            </h3>
+<h3>
+    ${escapeHTML(song.artist || "")}
+</h3>
 
-
-            <p class="artist">
-                ${escapeHTML(song.artist || "")}
-            </p>
+<p class="artist">
+    ${escapeHTML(song.title)}
+</p>
 
 
             <div class="tags">
